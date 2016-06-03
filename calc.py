7d684366ac70b2a11edd48cfdee1212e8832f08d
Audric D'Hoest <audricd@gmail.com>
2016-06-03 02:04:19 +0200

    v1.1
    code cleanup

--- calc.py
+++ calc.py
@@ -1,39 +1,48 @@
-"""Phantasy Star Online (1) ID calculator"""
+"""Phantasy Star Online (1) ID calculator
+https://github.com/audricd/PSOidCalc
+Tested on Windows 10 with Python 3.5.1
+"""
+import os
 
+fullcharname = input(str("Type in desired character name:"))  # grabs character name
+# charnamelen = len(fullcharname) TODO while for len exceeds character limiter
 
-fullcharname = input(str("Character name:"))
-charnamelen = len(fullcharname)
+charnums = (fullcharname.translate(str.maketrans('ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+                                                 'abcdefghijklmnopqrstuvwxyz'
+                                                 '1234567890'
+                                                 '`~!@#$%^&*()-_=+\|[{]};:\'\",<.>/? ',
+                                                 '56789012345678901234567890'
+                                                 '78901234567890123456789012'
+                                                 '9012345678'
+                                                 '963456748201551325133498644062732')))  # translation for char id
 
-charnums = (fullcharname.translate(str.maketrans('ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890'
-                                           '`~!@#$%^&*()-_=+\|[{]};:\'\",<.>/? ',
-                                           '567890123456789012345678907890123456789012345678'
+separado = [int(i) for i in str(charnums)]  # separates each character
+suma = sum(separado)  # sums each character value
+sumastr = str(suma)  # converts sum to string
+lastdig = (sumastr[-1])  # grabs the last digit of the sum
 
-                                           '90129012345678963456748201551325133498644062732')))
-
-separado = [int(i) for i in str(charnums)]
-suma = sum(separado)
-sumastr = str(suma)
-lastdig = (sumastr[-1])
-
+# translate last digit to game id
 if lastdig == "0":
-    print (fullcharname +" will generate a Virdia ID"),
+    print(fullcharname + " will generate a Viridia ID"),
 elif lastdig == "1":
-    print (fullcharname +" will generate a Greenile ID"),
+    print(fullcharname + " will generate a Greenill ID"),
 elif lastdig == "2":
-    print (fullcharname +" will generate a Skyly ID"),
+    print(fullcharname + " will generate a Skyly ID"),
 elif lastdig == "3":
-    print (fullcharname +" will generate a Bluefull ID"),
+    print(fullcharname + " will generate a Bluefull ID"),
 elif lastdig == "4":
-    print (fullcharname +" will generate a Purplenum ID"),
+    print(fullcharname + " will generate a Purplenum ID"),
 elif lastdig == "5":
-    print (fullcharname +" will generate a Pinkal ID"),
+    print(fullcharname + " will generate a Pinkal ID"),
 elif lastdig == "6":
-    print (fullcharname +" will generate a Redria ID"),
+    print(fullcharname + " will generate a Redria ID"),
 elif lastdig == "7":
-    print(fullcharname +" will generate a Oran ID"),
+    print(fullcharname + " will generate a Oran ID"),
 elif lastdig == "8":
-    print(fullcharname +" will generate a Yellowboze ID"),
+    print(fullcharname + " will generate a Yellowboze ID"),
 elif lastdig == "9":
-    print(fullcharname +" will generate a Whitill ID")
+    print(fullcharname + " will generate a Whitill ID")
 else:
     print("error")
+
+os.system("pause")  # prevents the window from closing
